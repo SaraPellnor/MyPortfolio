15b476b01cc12eea14cfe0924474f77b24b9892a
SaraPellnor <sara.ahlquist@hotmail.com>
2024-04-24 16:32:50 +0200

    solvd the problem with download

--- app/Components/ResumeActive.jsx
+++ app/Components/ResumeActive.jsx
@@ -1,10 +1,11 @@
 import CV from "../assets/cv.png"
 import Image from "next/image"
 const ResumeActive = () => {
+  console.log(CV);
   return (
     <div className=" relative flex justify-center items-center">
             <Image className="h-full w-auto scale-110" src={CV} alt="CV" />
-            <a className="absolute right-0 top-0 text-sm p-3 hover:scale-110 duration-500" href={CV} download> Download Resume </a>
+            <a className="absolute right-0 top-0 text-sm p-3 hover:scale-110 duration-500" href={CV.src} download > Download Resume </a>
 
 
     </div>
