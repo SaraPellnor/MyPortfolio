f8c88fb8f4a72b760b2bccec41e1148dca14389b
SaraPellnor <sara.ahlquist@hotmail.com>
2024-04-24 15:49:02 +0200

    fixt error

--- app/Components/AboutActive.jsx
+++ app/Components/AboutActive.jsx
@@ -3,11 +3,11 @@ const AboutActive = () => {
     <div className="overflow-hidden overflow-y-scroll scrollbar-track-gray-200 scrollbar-thumb-gray-500 hover:scrollbar-thumb-gray-600 h-full text-sm p-3">
       <h2 className="text-xl font-bold mb-4">About Me</h2>
       <p className="mb-6">
-        Hey there! I'm Sara, and I'm passionate about creating digital
+        {`Hey there! I'm Sara, and I'm passionate about creating digital
         experiences that leave a lasting impression. While I may not be an
         experienced developer by traditional standards, my enthusiasm,
         curiosity, and determination make me a force to be reckoned with in the
-        world of code.{" "}
+        world of code.`}
       </p>
 
       <h2 className="text-xl font-bold mb-4">Personal Skills</h2>
@@ -22,39 +22,39 @@ const AboutActive = () => {
 
       <h2 className="text-xl font-bold mb-4">Developing Skills</h2>
       <p className="mb-6">
-        In the realm of development, I've explored various technologies and
+       { `In the realm of development, I've explored various technologies and
         frameworks, including JavaScript, React, Node.js, and Express. While my
         journey is still in its infancy, I've already built a solid foundation
-        in web development and am eager to continue refining my skills.
+        in web development and am eager to continue refining my skills.`}
       </p>
 
       <h2 className="text-xl font-bold mb-4">Secret Skills</h2>
       <p className="mb-6">
-        Now let's talk about my secret weapon: my inherent creativity. Whether
+       { `Now let's talk about my secret weapon: my inherent creativity. Whether
         it's finding innovative solutions to complex problems or dreaming up
         captivating user experiences, I bring a unique perspective to every
         project I tackle. I thrive on thinking outside the box and pushing the
-        boundaries of what's possible.
+        boundaries of what's possible.`}
       </p>
 
       <h2 className="text-xl font-bold mb-4">Deepest Wishes</h2>
       <p className="mb-6">
-        My ultimate goal is to leverage my skills and passion for technology to
+       { `My ultimate goal is to leverage my skills and passion for technology to
         make a positive impact on the world around me. Whether it's building
         user-friendly applications that simplify everyday tasks or contributing
         in some small way to larger projects, I strive to leave behind work that
-        I can be proud of.
+        I can be proud of.`}
       </p>
 
       <h2 className="text-xl font-bold mb-4">Fun Facts</h2>
       <ul>
         <li className="mb-2">
-          When I'm not diving into code, you can often find me singing in the
-          choir or testing my strength in tug-of-war.
+         {` When I'm not diving into code, you can often find me singing in the
+          choir or testing my strength in tug-of-war.`}
         </li>
         <li className="mb-2">
-          I'm also a proud youth leader in the world of tug-of-war, where I help
-          mentor the next generation of pullers in a positive way.
+         {` I'm also a proud youth leader in the world of tug-of-war, where I help
+          mentor the next generation of pullers in a positive way.`}
         </li>
         <li className="mb-2">
           Despite my love for technology, I cherish moments of peace and
