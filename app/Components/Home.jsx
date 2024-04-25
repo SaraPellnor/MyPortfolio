16560bde2c4b7732ee0a5520efd09c4fadda3893
SaraPellnor <sara.ahlquist@hotmail.com>
2024-04-25 13:56:49 +0200

    style={{ height: `${screenHeight}px` }}

--- app/Components/Home.jsx
+++ app/Components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { GiCompass } from "react-icons/gi";
 import Compass from "./Compass";
 import About from "./About";
@@ -6,41 +6,14 @@ import Projects from "./Projects";
 import Resume from "./Resume";
 import Connect from "./Connect";
 const Home = () => {
+
   const [hoverd, setHoverd] = useState("");
-  const useWindowDimensions = () => {
-    const hasWindow = typeof window !== "undefined";
-
-    function getWindowDimensions() {
-      const height = hasWindow ? window.innerHeight : null;
-      return {
-        height,
-      };
-    }
-
-    const [windowDimensions, setWindowDimensions] = useState(
-      getWindowDimensions()
-    );
-
-    useEffect(() => {
-      if (hasWindow) {
-        function handleResize() {
-          setWindowDimensions(getWindowDimensions());
-        }
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-      }
-    }, [hasWindow]);
-
-    return windowDimensions;
-  };
-
-  const { height } = useWindowDimensions();
+  const hasWindow = typeof window !== "undefined";
+console.log(hasWindow);
+  const screenHeight = hasWindow ? window.innerHeight : 844.
+  
   return (
-    <div
-      style={{ height: `${height}px` }}
-      className={`flex flex-wrap w-full md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer`}
-    >
+    <div style={{ height: `${screenHeight}px` }} className={`flex flex-wrap w-full md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer`}>
       <Compass setHoverd={setHoverd} hoverd={hoverd} content={<GiCompass />} />
       <About setHoverd={setHoverd} hoverd={hoverd} content={"About"} />
       <Projects setHoverd={setHoverd} hoverd={hoverd} content={"Projects"} />
