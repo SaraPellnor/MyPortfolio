d8be94a015e64ff765adf42de2e8d07bb748950c
SaraPellnor <sara.ahlquist@hotmail.com>
2024-04-25 11:50:45 +0200

    to 95vh

--- app/Components/Home.jsx
+++ app/Components/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
   const [hoverd, setHoverd] = useState("");
 
   return (
-    <div className="flex flex-wrap w-full h-[90vh]  md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer">
+    <div className="flex flex-wrap w-full h-[95vh]  md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer">
       <Compass setHoverd={setHoverd} hoverd={hoverd} content={<GiCompass />} />
       <About setHoverd={setHoverd} hoverd={hoverd} content={"About"} />
       <Projects setHoverd={setHoverd} hoverd={hoverd} content={"Projects"} />
