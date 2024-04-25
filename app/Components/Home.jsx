3976dfbcea3d7fa810a0afd3f4590feeaf4a62b3
SaraPellnor <sara.ahlquist@hotmail.com>
2024-04-25 14:23:43 +0200

    if (typeof window !== "undefined") {

--- app/Components/Home.jsx
+++ app/Components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GiCompass } from "react-icons/gi";
 import Compass from "./Compass";
 import About from "./About";
@@ -6,14 +6,29 @@ import Projects from "./Projects";
 import Resume from "./Resume";
 import Connect from "./Connect";
 const Home = () => {
-
   const [hoverd, setHoverd] = useState("");
-  const hasWindow = typeof window !== "undefined";
-console.log(hasWindow);
-  const screenHeight = hasWindow ? window.innerHeight : window.innerHeight
-  
+  const [screenHeight, setScreenHeight] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setScreenHeight(window.innerHeight);
+
+      const handleResize = () => {
+        setScreenHeight(window.innerHeight);
+      };
+      window.addEventListener("resize", handleResize);
+
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
   return (
-    <div style={{ height: `${screenHeight}px` }} className={`flex flex-wrap w-full md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer`}>
+    <div
+      style={{ height: `${screenHeight}px` }}
+      className={`flex flex-wrap w-full md:h-[100vh] font-400 font-josefin-sans md:text-6xl text-4xl border-solid border-[2px] border-black bg-black relative cursor-pointer`}
+    >
       <Compass setHoverd={setHoverd} hoverd={hoverd} content={<GiCompass />} />
       <About setHoverd={setHoverd} hoverd={hoverd} content={"About"} />
       <Projects setHoverd={setHoverd} hoverd={hoverd} content={"Projects"} />
